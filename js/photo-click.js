3e183288138033fe6d41ec786d0b68c15fea6bce
Maarten Van Malcot <maarten.van.malcot@student.ehb.be>
2018-06-24 20:14:09 +0200

    update

---
+++ js/photo-click.js
@@ -0,0 +1,9 @@
+
+  // Handler for .ready() called.
+    function photoclick () {
+        
+        
+        
+        window.location.href = 'photography-full-size?folder='+folder+'&id=' + id;
+         console.log('lick');
+    }
